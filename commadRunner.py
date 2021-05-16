a3a96ccb9ed1c0bdd5d41d77f94cb9e1f94efd5a
pramodelangovan <pramood46@gmail.com>
2021-05-16 19:55:44 +0530

    staging

--- commadRunner.py
+++ commadRunner.py
@@ -60,14 +60,14 @@ def handle(msg):
                     stats = getSystemInfo()
                     bot.sendMessage(senderChatId, stats)
                     time.sleep(10)
-                elif command.startswith('brightness'):
-                       brightVal = int(command.replace('brightness', '').strip())
-                       setBrightness(brightVal)
-                       bot.sendMessage(senderChatId, 'Brightness set to {}%'.format(brightVal))
-                elif command == 'click':
-                       fileName = getImage()
-                       bot.sendPhoto(senderChatId, open(fileName, "rb"))
-                       os.remove(fileName)
+                # elif command.startswith('brightness'):
+                #        brightVal = int(command.replace('brightness', '').strip())
+                #        setBrightness(brightVal)
+                #        bot.sendMessage(senderChatId, 'Brightness set to {}%'.format(brightVal))
+                # elif command == 'click':
+                #        fileName = getImage()
+                #        bot.sendPhoto(senderChatId, open(fileName, "rb"))
+                #        os.remove(fileName)
                 elif  command == 'gold':
                     rates = getCurrentGoldRatesByCity()
                     bot.sendMessage(senderChatId, rates, parse_mode="HTML")
